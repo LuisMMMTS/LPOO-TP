5747c6354f19a531fe2fbedd82ae1f12df0397df
LuisMMMTS <up201808912@fe.up.pt>
2020-02-28 17:36:01 +0000

    draw in x,y position

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,20 +2,26 @@ import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() throws IOException {
-        terminal= new DefaultTerminalFactory().createTerminal();
+        Terminal terminal= new DefaultTerminalFactory().createTerminal();
         this.screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.setCharacter(x, y, new TextCharacter('X'));
         screen.refresh();
     };
     public void run() throws IOException {
